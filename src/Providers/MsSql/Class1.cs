bad1bc2b0fa12df43ce7b197709892e460309c96
Roman Metlinskyi <gmail@metlinskyi.com>
2020-05-02 15:04:57 +0200

    mssql and mysql clients

--- src/Providers/MsSql/Class1.cs
+++ src/Providers/MsSql/Class1.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace MsSql
 {
     public class Class1
     {
+        public void A()
+        {
+            using (var connection = new SqlConnection("Server=tcp:localhost,1433;User Id=sa;Password=Passw0rd!"))
+            {
+                var command = new SqlCommand("SELECT * FROM [A].[Entity].[Customers]", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}:{reader[1]} ${reader[2]}");
+                    }
+                }
+            }
+        }
     }
 }
--- src/Providers/MySql/Class1.cs
+++ src/Providers/MySql/Class1.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace MySql
 {
     public class Class1
     {
+        public void A()
+        {
+            using (var connection = new MySqlConnection
+                {
+                    ConnectionString = "server=localhost;user id=root;password=******;persistsecurityinfo=True;port=3305;database=music"
+                }) {
+                connection.Open();
+                var command = new MySqlCommand("SELECT * FROM music.category;", connection);
+
+                using (MySqlDataReader reader =  command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(
+                            $"{reader["category_id"]}: {reader["name"]} {reader["last_update"]}");
+                    }
+                }
+            }
+        }
     }
 }
